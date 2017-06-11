0a56f7b6395e3cc026a4063bfa741029f2fcaf42
lgybetter <437675103@qq.com>
2017-06-12 01:39:57 +0800

    test

---
+++ src/components/Hello.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <vue-table>
+      <button slot="action">test</button>
+    </vue-table>
+  </div>
+</template>
+
+<script>
+import vueTable from './table'
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    vueTable
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/table.vue
@@ -0,0 +1,47 @@
+<template>
+  <table>
+    <thead ref="thead">
+      <tr>
+        <th>Month</th>
+        <th>Saving</th>
+      </tr>
+    </thead>
+    <tbody ref="tbody">
+      <tr v-for="data in datas">
+        <td>{{data.month}}</td>
+        <td>{{data.saving}}</td>
+      </tr>
+    </tbody>
+    <slot name="action"></slot>
+  </table>
+</template>
+
+<script>
+  export default {
+    mounted () {
+      console.log(this.$slots.action[0].tag)
+    },
+    data () {
+      return {
+        datas: [
+          { 
+            month: 1,
+            saving: 12423
+          },
+          { 
+            month: 1,
+            saving: 12423
+          },
+          { 
+            month: 1,
+            saving: 12423
+          },
+          { 
+            month: 1,
+            saving: 12423
+          }          
+        ]
+      }
+    }
+  }
+</script>
